909493b10fac64a564d53b32dad8c1eaab9708a9
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-18 11:42:32 -0500

    Add rooms method

--- lib/house.rb
+++ lib/house.rb
@@ -1,13 +1,22 @@
 class House
-    attr_accessor :price, :address
+    attr_accessor :price, :address, :rooms 
 
   def initialize(price, address)
       @price = price
       @address = address
+      @rooms = []
   end
 
   def price
     @price.delete!('$')
       @price.to_i
+  end
+
+  def rooms 
+    @rooms
+  end
+
+  def add_room(room)
+    @rooms.push(room)
   end
 end
